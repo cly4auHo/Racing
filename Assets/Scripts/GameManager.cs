c02525898d0904465f6c2b07fafef41f9da1df86
cly4auHo <arsen.dmitriev96@gmail.com>
2020-11-10 05:36:56 +0200

    add mini map

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject pause;
+
+    private void Start()
+    {
+        Time.timeScale = 0;
+        menu.SetActive(true);
+        pause.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            pause.SetActive(true);
+        }
+    }
+
+    public void Play()
+    {
+        menu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ShowScore()
+    {
+
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pause.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
