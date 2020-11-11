40fb317167c668882eaba8dd4c7e7cfa76120418
cly4auHo <arsen.dmitriev96@gmail.com>
2020-11-11 11:14:17 +0200

    controller transform

--- Assets/Scripts/KeyBoardController.cs
+++ Assets/Scripts/KeyBoardController.cs
@@ -3,26 +3,63 @@ using UnityEngine.UI;
 
 public class KeyBoardController : MonoBehaviour
 {
-    private CharacterController charController;
-    [SerializeField] private float speed = 6;
+    [SerializeField] private float deltaSpeed = 0.1f;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float pressCoef = 5;
+    [SerializeField] private float rotationSpeed = 6;
     [SerializeField] private Text speedLabel;
-    private float rotationY;
+    private float speed;
 
     private void Start()
     {
-        charController = GetComponent<CharacterController>();
+        speed = minSpeed;
     }
 
     private void Update()
     {
-        float deltaX = Input.GetAxis("Horizontal") * speed;
-        float deltaZ = Input.GetAxis("Vertical") * speed;
+        if (Input.GetKey(KeyCode.W))
+        {
+            speed = Mathf.Min(maxSpeed, speed + deltaSpeed * pressCoef);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (speed > 0)
+            {
+                speed = Mathf.Max(minSpeed, speed - deltaSpeed * pressCoef);
+            }
+            else
+            {
+                speed = Mathf.Max(-maxSpeed, speed - deltaSpeed * pressCoef);
+            }
+        }
+        else
+        {
+            if (speed >= 0)
+            {
+                speed = Mathf.Max(minSpeed, speed - deltaSpeed);
+            }
+            else
+            {
+                speed = Mathf.Min(maxSpeed, speed + deltaSpeed);
+            }
+        }
 
-        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-        movement = Vector3.ClampMagnitude(movement, speed);
-        movement = transform.TransformDirection(movement);
-        movement *= Time.deltaTime;
+        string velovity = speed.ToString();
 
-        charController.Move(movement);
+        if (velovity.Contains(",") || velovity.Contains(".")) //show not all digits, only 2 after dot
+            velovity = string.Format("{0:0.00}", speed);
+
+        speedLabel.text = velovity;
+
+        //sd inputs for rotation
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.Translate(0, 0, speed * Time.deltaTime);
+
+        //corrert rotation if move back
+        if (speed > 0)
+            transform.Rotate(0, rotation, 0);
+        else if (speed < 0)
+            transform.Rotate(0, -rotation, 0);
     }
 }
