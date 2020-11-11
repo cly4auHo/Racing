39da0456a6724260406aba37ff3948c3a4623714
cly4auHo <arsen.dmitriev96@gmail.com>
2020-11-11 12:02:01 +0200

    rigidBody movment

--- Assets/Scripts/KeyBoardController.cs
+++ Assets/Scripts/KeyBoardController.cs
@@ -9,10 +9,12 @@ public class KeyBoardController : MonoBehaviour
     [SerializeField] private float pressCoef = 5;
     [SerializeField] private float rotationSpeed = 6;
     [SerializeField] private Text speedLabel;
+    private Rigidbody rb;
     private float speed;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody>();
         speed = minSpeed;
     }
 
@@ -51,15 +53,17 @@ public class KeyBoardController : MonoBehaviour
             velovity = string.Format("{0:0.00}", speed);
 
         speedLabel.text = velovity;
+    }
 
-        //sd inputs for rotation
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
-        transform.Translate(0, 0, speed * Time.deltaTime);
+    private void FixedUpdate()
+    {
+        rb.AddRelativeForce(Vector3.forward * speed);
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed; //sd inputs for rotation
 
         //corrert rotation if move back
         if (speed > 0)
-            transform.Rotate(0, rotation, 0);
+            rb.AddTorque(new Vector3(0, rotation, 0));
         else if (speed < 0)
-            transform.Rotate(0, -rotation, 0);
+            rb.AddTorque(new Vector3(0, -rotation, 0));
     }
 }
